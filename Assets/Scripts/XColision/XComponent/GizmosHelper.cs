abbe0f9fb487cd22e8ff8b2bfd8c87ebd681a10a
wo_fpc <wo_fpc@outlook.com>
2017-11-10 17:24:02 +0800

    init

---
+++ Assets/Scripts/XColision/XComponent/GizmosHelper.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace XColision.XComponent
+{
+    public class GizmosHelper
+    {
+        public static void DrawCircle(Vector3 center, float radius)
+        {
+            var p = center + Vector3.right * radius;
+            for (int i = 0; i < 51; i++)
+            {
+                var theta = 2 * Mathf.PI * i / 50f;
+                var t = center + new Vector3(Mathf.Cos(theta), 0, Mathf.Sin(theta)) * radius;
+                Gizmos.DrawLine(p, t);
+                p = t;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/XColision/XComponent/XCollider.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace XColision.XComponent
+{
+    public abstract class XCollider : MonoBehaviour
+    {
+
+        protected virtual void OnDrawGizmosSelected()
+        {
+            
+        }
+    }
+}
